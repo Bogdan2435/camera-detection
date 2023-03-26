5a4270af8b308bb02fd9357afd86f8a25812b2d7
Dura Bogdan <bogdan2435@gmail.com>
2023-03-27 00:20:53 +0300

    small adjustments

--- object-detection.py
+++ object-detection.py
@@ -12,12 +12,11 @@ while True:
     res1 = model1(img, device='mps') # predict for first model, replace mps with cuda for nvidia gpu
     res_plotted = res1[0].plot() # plot results for first model 
 
-
     res2 = model2(img, device='mps') # predict for second model, replace mps with cuda for nvidia gpu
     res2_plotted = res2[0].plot() # plot results for second model
 
-    cv2.imshow("Yolo v8", res_plotted) # show results for first model
-    cv2.imshow("Yolo v5", res2_plotted) # show results for second model
+    cv2.imshow("Yolo v8 - first model", res_plotted) # show results for first model
+    cv2.imshow("Yolo v5 - second model", res2_plotted) # show results for second model
 
     if cv2.waitKey(1) & 0xFF == ord('q'): # press q to stop
         break
